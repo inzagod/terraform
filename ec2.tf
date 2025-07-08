175ef5ae28a9e58b3c76bf23c5cc670abd98eb06
ludovic lachevre <42611540+inzagod@users.noreply.github.com>
2025-07-08 16:40:15 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region     = "us-east-1"
+}
 
+resource "aws_instance" "myec2" {
+  ami                    = "ami-05ffe3c48a9991133"
+  instance_type          = "t2.micro"
+  key_name               = "devops-ludovic"
+  tags = {
+    Name = "tfm-tag"
+  }
+}
